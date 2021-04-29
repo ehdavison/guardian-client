67dd0fd83f98e378c9d4cd48ea3dadcb95a1a0f5
ehdavison <elijahdavison2@gmail.com>
2021-04-29 14:45:46 -0500

    added task index

---
+++ src/api/tasks.js
@@ -0,0 +1,54 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const indexTasks = (user) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/tasks',
+    headers: {
+      'Authorization': `Bearer ${user}`
+    }
+  })
+}
+
+export const showTask = (user, taskId) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/tasks/' + taskId,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+export const deleteTask = (user, taskId) => {
+  return axios({
+    method: 'DELETE',
+    url: apiUrl + '/tasks/' + taskId,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+export const createTask = (user, task) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/tasks',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: { task }
+  })
+}
+
+export const updateTask = (user, task, id) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/tasks/' + id,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: { task: task }
+  })
+}
