15a2ffe726b19ca34116567be333b35b393da411
ehdavison <elijahdavison2@gmail.com>
2021-06-11 15:23:29 -0500

    added fields with appropriate state

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {useState} from 'react';
 
-const TaskCreate = () => {
+const TaskCreate = (props) => {
+    const [title, setTitle] = useState();
+    const [location, setLocation] = useState();
+    const [time, setTime] = useState();
+    const [details, setDetails] = useState();
+    const [completed, setCompleted] = useState(0);
+
     return (
         <div>
-            <p>This is the task create page</p>
+            <form>
+                <h2>Create a task!</h2>
+                <label className='title-input' for='title'>Title</label>
+                <input className='title-input' name='title' type='text' placeholder='Title' onChange={(event) => setTitle(event.target.value)}></input>
+
+                <label className='location-input' for='location'>Location</label>
+                <input className='location-input' name='location' type='text' placeholder='Location' onChange={(event) => setLocation(event.target.value)}></input>
+
+                <label className='time-input' for='time'>Time</label>
+                <input className='time-input' name='time' type='text' placeholder='Time' onChange={(event) => setTime(event.target.value)}></input>
+
+                <label className='details-input' for='details'>Details</label>
+                <input className='details-input' name='details' type='text' placeholder='Details' onChange={(event) => setDetails(event.target.value)}></input>
+
+                <label className='completed-input' for='completed'>Times Completed</label>
+                <input className='completed-input' name='completed' type='number' placeholder='Times Completed' onChange={(event) => setCompleted(event.target.value)}></input>
+                
+
+            </form>
         </div>
     );
 };
